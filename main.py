1a6c5fc8788da2c29d03d7a380ea10926b0ad492
Mohammed Hussen <99973399+awash-dev@users.noreply.github.com>
2025-01-10 16:00:17 +0300

    Update main.py
    Add cors

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from chapaConfig import Payment
 from pydantic import BaseModel, EmailStr, PositiveInt
 
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
